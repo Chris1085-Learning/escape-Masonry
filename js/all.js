af3560d359ab659e039f2abec188920d56c62cf1
Chris Chiang <itscfchiang@gmail.com>
2022-03-28 10:44:47 +0800

    fix: fix animation style

--- js/all.js
+++ js/all.js
@@ -1,12 +1,26 @@
-var $grid = $('.grid').imagesLoaded(function () {
-  $grid.masonry({
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true,
-    getter: 10
-  })
-})
+const $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-item',
+  percentPosition: true,
+  gutter: 8
+});
 
-$grid.imagesLoaded().progress(function () {
-  $grid.masonry()
-})
+const images = document.querySelectorAll('.grid-item img');
+
+let num = 0;
+images.forEach((el) => {
+  const img = new Image();
+  img.src = el.src;
+  img.addEventListener('load', () => {
+    num += 1;
+    if (images.length === num) {
+      $grid.masonry();
+      images.forEach((item, i) => {
+        setTimeout(() => {
+          item.classList.add('scaleUp');
+          console.log(i);
+        }, 300 * i);
+      });
+    }
+  });
+});
